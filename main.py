c8f996a5f7a420ae7bbb18352ca29b5cb64943e1
hubercikk47 <hubert.niespial@onet.pl>
2025-07-17 18:50:20 +0200

    Adding create user route and validation

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, APIRouter, status
+from typing_extensions import Annotated
+from schemas.user import user_create, user_out, user_login
+from schemas.product import product_create, product_delete, product_update
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from models import product, user
+from passlib.context import CryptContext
+
 
 app = FastAPI()
+router = APIRouter()
+user.Base.metadata.create_all(bind=engine)
+product.Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/createuser/", status_code=status.HTTP_201_CREATED, response_model=user_out)
+async def create_user(user: user_create.UserCreate, db: db_dependency):
+    existing = db.query(user.User).filter(user.User.email == user.email).first()
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail="User with this e-mail address already exists"
+        )
